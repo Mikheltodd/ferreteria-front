cc26e7c9d8237cf52a1a1c2a855a4b45e67c065a
mikheltodd <miguel.ortiz11235@gmail.com>
2021-08-30 22:12:21 -0500

    Basic Styling Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,50 @@
 <template>
   <div id="app" class="app">
-    <div class="header">
-      <h1>Banco UN</h1>
-
-      <nav>
-        <button v-on:click="init" v-if="is_auth">Inicio</button>
-        <button v-on:click="account" v-if="is_auth">Productos</button>
-        <button v-on:click="transacction" v-if="is_auth">Transacciones</button>
-        <button v-on:click="historial" v-if="is_auth">Eliminar</button>
-        <button v-on:click="logOut" v-if="is_auth">Cerrar Sesión</button>
+    <header>
+      <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand logotype" href="#">
+            <img src="../public/assets/Logo.png" alt="Logo Ferretería" />
+            <h1>Ferretería</h1>
+          </a>
+          <ul class="nav nav-tabs nav-fill">
+            <li class="nav-item">
+              <a class="nav-link" v-on:click="init" v-if="is_auth">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" v-on:click="account" v-if="is_auth"
+                >Productos</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" v-on:click="transacction" v-if="is_auth"
+                >Transacciones</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" v-on:click="historial" v-if="is_auth"
+                >Eliminar</a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" v-on:click="logOut" v-if="is_auth"
+                >Cerrar Sesión</a
+              >
+            </li>
+          </ul>
+        </div>
       </nav>
-    </div>
+    </header>
 
     <div class="main-component">
       <router-view v-on:log-in="logIn"></router-view>
     </div>
 
-    <div class="footer">
-      <h2>Misión TIC 2022</h2>
-    </div>
+    <footer
+      class="flex-wrap justify-content-between align-items-center bg-dark"
+    >
+      <span class="text-white">&copy; 2021 Company, Inc</span>
+    </footer>
   </div>
 </template>
 
@@ -121,84 +147,5 @@ export default {
 </script>
 
 <style>
-body {
-  margin: 0 0 0 0;
-}
-
-.header {
-  position: fixed;
-  left: 0px;
-  top: 0px;
-  width: 100%;
-  height: 100px;
-  margin: 0%;
-  padding: 0;
-
-  background-color: #283747;
-  color: #e5e7e9;
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.header h1 {
-  width: 20%;
-  text-align: center;
-}
-
-.header nav {
-  height: 100%;
-  width: 40%;
-
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-
-  font-size: 20px;
-}
-
-.header nav button {
-  color: #e5e7e9;
-  background: #283747;
-  border: 1px solid #e5e7e9;
-
-  border-radius: 5px;
-  padding: 10px 20px;
-}
-
-.header nav button:hover {
-  color: #283747;
-  background: #e5e7e9;
-  border: 1px solid #e5e7e9;
-}
-
-.main-component {
-  height: 75vh;
-  margin: 0%;
-  padding: 0%;
-
-  background: #fdfefe;
-}
-
-.footer {
-  position: fixed;
-  left: 0px;
-  bottom: 0px;
-  width: 100%;
-  height: 75px;
-
-  background-color: #283747;
-  color: #e5e7e9;
-}
-
-.footer h2 {
-  margin: 0px;
-  width: 100%;
-  height: 100%;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+@import "../public/assets/styles/main.css";
 </style>
